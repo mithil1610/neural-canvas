0cbae0361fbda16844300f6336e17e31e174bdf0
Mithil Patel <mithilpatel140@gmail.com>
2026-07-08 16:01:27 -0500

    fix(ios): point background target manifests to group.com.mithil.axiom to clear runtime handleMedia crash

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -3,8 +3,6 @@ import Social
 import receive_sharing_intent
 
 class ShareViewController: RSIShareViewController {
-    // Set to true to automatically skip the iOS compose window 
-    // and route the image directly into your Flutter application layout.
     override func shouldAutoRedirect() -> Bool {
         return true
     }

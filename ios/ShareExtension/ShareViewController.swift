f29bf214dc2ccf06e88a9967076a6c118e416899
Mithil Patel <mithilpatel140@gmail.com>
2026-07-08 21:37:45 -0500

    fix(ios): map CUSTOM_GROUP_ID settings and property arrays to close extension crashes

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -4,6 +4,6 @@ import receive_sharing_intent
 
 class ShareViewController: RSIShareViewController {
     override func shouldAutoRedirect() -> Bool {
-        return true
+        return false
     }
 }

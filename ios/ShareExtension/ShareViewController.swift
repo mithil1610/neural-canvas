54c66dafaf92e19d9544b85b7e3cca9596b3a825
Mithil Patel <mithilpatel140@gmail.com>
2026-07-08 22:20:23 -0500

    fix(ios): set shouldAutoRedirect to true and register unified URL schemes

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -4,6 +4,6 @@ import receive_sharing_intent
 
 class ShareViewController: RSIShareViewController {
     override func shouldAutoRedirect() -> Bool {
-        return false
+        return true
     }
 }
